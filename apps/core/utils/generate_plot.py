f27a4b35934b9b5ca2bdb2c68b9d45dfe1ad87aa
ngutierrez <nicolas.gutierrez@soundtalks.com>
2023-03-21 16:48:35 +0100

    Fix bug

--- apps/core/utils/generate_plot.py
+++ apps/core/utils/generate_plot.py
@@ -3,5 +3,17 @@ import plotly.express as px
 
 
 def generate_plot(df):
-    fig = px.line(df, template='seaborn', markers=True)
+    fig = px.line(df,
+                  template='seaborn',
+                  markers=True,
+                  title="Exertion for exercise",
+
+                  labels={
+                      "data_count": "1/60 Seconds",
+                      "_value": "Exertion",
+                  },
+
+                  )
+    fig.update_layout(yaxis_range=[0.1, 1])
+
     return plot(fig, output_type='div')
