3b2ac39fdafea8e93c492dbdb5253da125b75011
ngutierrez <nicolas.gutierrez@soundtalks.com>
2023-03-27 12:21:33 +0200

    Removed unused views

--- apps/api_auth/views.py
+++ apps/api_auth/views.py
@@ -13,57 +13,6 @@ from .serializers import ExerciseSerializer, DeviceSerializer, ListDatumSerializ
 from ..core.models import Datum, Exercise, Device
 
 
-# dario
-
-
-class SessionView(APIView):
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    @staticmethod
-    def get(request):
-        return JsonResponse({"isAuthenticated": True})
-
-
-class WhoAmIView(APIView):
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    @staticmethod
-    def get(request):
-        return JsonResponse({"username": request.user.username, "id": request.user.id})
-
-
-class LogoutView(APIView):
-    # This view should be accessible also for unauthenticated users.
-    permission_classes = [permissions.AllowAny]
-    allowed_methods = ("POST",)
-
-    @staticmethod
-    def post(request):
-        if not request.user.is_authenticated:
-            return JsonResponse({"detail": "You're not logged in."}, status=400)
-
-        logout(request)
-        return JsonResponse({"detail": "Successfully logged out."})
-
-
-class LoginView(generics.GenericAPIView):
-    # This view should be accessible also for unauthenticated users.
-    permission_classes = [permissions.AllowAny]
-    serializer_class = serializers.LoginSerializer
-    allowed_methods = ("POST",)
-
-    def post(self, request):
-        serializer = self.serializer_class(
-            data=self.request.data, context={"request": self.request}
-        )
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-        login(request, user)
-        return Response(None, status=status.HTTP_202_ACCEPTED)
-
-
 class DeviceView(mixins.RetrieveModelMixin, generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = DeviceSerializer
